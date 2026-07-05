75bcaa0afcfa59a5d90bc897f4f426a1eec92781
louannem <84766775+louannem@users.noreply.github.com>
2026-07-05 14:34:12 +0200

    add: define app configuration with UI color settings

---
+++ app/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: "mauve",
+      secondary: "slate",
+      error: "rose",
+      warning: "orange",
+      success: "green",
+    }
+  }
+})
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,47 +1,56 @@
 <template>
-  <UScrollArea 
-    id="dashboard"
-    v-slot="{ item, index }" 
-    :items="cards" 
-    orientation="vertical"
-    :virtualize="{
-      gap: 16,
-      lanes: 3,
-      estimateSize: 300
-    }"
-  >
-  <UPageCard
-    v-bind="item"
-    :style="{
-      height: `${item.height}px`,
-    }"
-  >
-
-  <template #footer>
-    <div v-if="item.user" class="blog-post-footer">
-      <UUser v-bind="item.user" />
-    </div>
-  </template>
-
-    
-  </UPageCard>
+  <div v-if="!loading" class="">
+    <UScrollArea 
+      id="dashboard"
+      v-slot="{ item, index }" 
+      :items="cards" 
+      orientation="vertical"
+      :virtualize="{
+        gap: 16,
+        lanes: 3,
+        estimateSize: 300
+      }"
+    >
+    <UPageCard
+      v-bind="item"
+      :style="{
+        height: `${item.height}px`,
+      }"
+    >
+  
+    <template #footer>
+      <div v-if="item.user" class="blog-post-footer">
+        <UUser v-bind="item.user" />
+      </div>
+    </template>
+  
+      
+    </UPageCard>
   </UScrollArea>
+  <UPageHero v-bind="heroAfterScroll" />
+  </div>
+  <div class="dashboard-skeleton">
+    <USkeleton v-for="card of cards" class="w-[250px]" :style="{
+      height: `${card.height}px`
+    }" />
+  </div>
 </template>
 
 <script setup>
-import { card } from '#build/ui';
 
   definePageMeta({
     name: "Dashboard",
     layout: "dashboard"
   });
 
+  let loading = ref(true);
+
   let cards = [
     {
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
       user: {
         name: "Anonymous user",
         avatar: "style: 'background-color: lightgrey; color: white'"
@@ -51,31 +60,31 @@ import { card } from '#build/ui';
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
     },
     {
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
     },
     {
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
     },
     {
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
     },
     {
       title: "Card",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus sodales odio, vel porta massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc vulputate finibus nulla nec ornare. Curabitur in efficitur justo. Vivamus ultrices magna odio, nec laoreet turpis pulvinar rhoncus.",
       to: "/",
-      icon: "i-simple-icons-tailwindcss",
+      icon: "i-lucide-book-open-text",
     },
   ];
 
@@ -117,6 +126,27 @@ import { card } from '#build/ui';
       height: getHeights(index),
       ...(cardUser ? { user: cardUser } : {})
     }
+  })
+
+  const heroAfterScroll = {
+    title: "Lorem ipsum",
+    description: "Nostrud ut exercitation ullamco reprehenderit. Officia elit qui aute incididunt cillum eu fugiat. Nostrud deserunt sint nostrud ipsum est mollit commodo aliqua duis cupidatat dolore nisi.",
+    links: [
+      {
+        label: "Sign in",
+        color: "primary",
+        trailingIcon: "i-lucide-arrow-right",
+        to: "/sign-in",
+      }
+    ]
+  }
+
+  onMounted(() => {
+    console.log(loading);
+    setTimeout(() => {
+      loading.value = false;
+      console.log(loading);
+    }, 1000 * 5);
   })
 
 </script>
