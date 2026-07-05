c36fba7fadf7eed78eea55a6601eb37ea77203d1
louannem <84766775+louannem@users.noreply.github.com>
2026-07-05 21:05:06 +0200

    add: login endpoint & logic

--- api.ts
+++ api.ts
@@ -1,5 +1,7 @@
 import { Elysia } from 'elysia'
+import login from '~/server/routes/auth/login'
 
 export default () =>
   new Elysia()
     .get('/hello', () => ({ message: 'Hello world!' }))
+    .use(login)
---
+++ app/server/routes/auth/login.ts
@@ -0,0 +1,31 @@
+import { Elysia, t } from 'elysia'
+import { CredentialsSchema, getUser } from '~/server/utils/session';
+
+const login = new Elysia()
+  .post(
+    '/login',
+    ({ body, status }) => {
+      const { username, password } = body as { username: string; password?: string };
+      const credentials = CredentialsSchema.safeParse(body);
+
+      if (!credentials) return status(500, "Username and password are required");
+
+      //Mock user login here
+      const user = getUser(username);
+
+      // Replace this placeholder logic with your real authentication flow.
+      if (username !== 'jane.doe@mail.com' || password !== 'azerty') {
+        return status(401, "Invalid credentials");
+      }
+
+      return status(200);
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String()
+      })
+    }
+  )
+
+export default login;
